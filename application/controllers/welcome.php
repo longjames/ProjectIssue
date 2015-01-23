2731a906ddef5f25a51ac2e7fb0bb0e862e3f715
carpela <carpela@163.com>
2015-01-23 12:09:15 +0800

    展示功能及搜索框架

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,14 +2,29 @@
 
 class Welcome extends CI_Controller {
 
-	public function login()
-	{
-		
-	}
 	public function index()
 	{
 		$this->load->model('project');
 		$data['projects'] = $this->project->getAll();
-		$this->load->view('list',$data);
+		$this->load->view('user/show',$data);
+	}
+
+	public function search()
+	{
+		$this->load->model('project');
+		$res = $this->project->getAll();
+		
+		$project = ();
+		foreach($res as $item)
+		{
+			//按照标题和描述筛选内容 分词等技术
+			if(true)
+			{
+				$project[] = $item;
+			}
+		}
+
+		$data['project'] = $project;
+		$this->load->view('user/show',$data);
 	}
 }
